3c1b9df844fd0105e612ecf5948b2fce68a8a35e
klaus <devgrimoire@gmail.com>
2020-05-03 01:41:16 +0530

    added src initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const port = process.env.port | 3000;
+
+app.use(express.static(path.join(__dirname,'/public')));
+
+app.get('',(req,res) => {
+    res.send('hello');
+})
+
+
+
+app.listen(port,()=> {
+    console.log(`Server running on port: ${port}`);
+})
