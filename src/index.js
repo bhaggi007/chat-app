764c72ffe52492e43f96014e6464ca7e210fd3bd
klaus <devgrimoire@gmail.com>
2020-05-03 02:42:54 +0530

    integrated socket.io

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-const path = require("path")
-const express = require("express")
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
+const publicDirectory = path.join(__dirname,'../public')
 
-app.use(express.static(path.join(__dirname,'../public')));
+app.use(express.static(publicDirectory));
 
+io.on('connection',()=>{
+    console.log('New socket connection')
+})
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log(`Chat app running on port: ${port}`);
 })
