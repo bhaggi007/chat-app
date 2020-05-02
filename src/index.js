7a4ee3cd62ff09d52252655b728a111fbcee1f57
klaus <devgrimoire@gmail.com>
2020-05-03 02:03:13 +0530

    committed with updated changes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
-const express = require('express');
-const path = require('path');
+const path = require("path")
+const express = require("express")
 
 const app = express();
 
-const port = process.env.port | 3000;
+const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname,'/public')));
-
-app.get('',(req,res) => {
-    res.send('hello');
-})
+app.use(express.static(path.join(__dirname,'../public')));
 
 
-
-app.listen(port,()=> {
-    console.log(`Server running on port: ${port}`);
+app.listen(port, ()=>{
+    console.log(`Chat app running on port: ${port}`);
 })
