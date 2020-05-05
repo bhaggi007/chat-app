f3c6cef74e9a9c6cb7e0622f69e80609513996ef
klaus <devgrimoire@gmail.com>
2020-05-05 18:18:57 +0530

    forms and buttons

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,9 +1,26 @@
 const socketio = io();
 
-document.querySelector('#chat-form').addEventListener('submit',(e) => {
+// Elements
+const $messageForm = document.querySelector('#chat-form');
+const $messageFormInput = document.querySelector('input')
+const $messageFormButton = document.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+
+socketio.on('message',(updatedMessage) => {
+    console.log(updatedMessage)
+})
+
+
+$messageForm.addEventListener('submit',(e) => {
     e.preventDefault();
+    // disable
+    $messageFormButton.setAttribute('disabled','disabled')
     const msg = e.target.elements.txtmsg.value;
     socketio.emit('sentMessage',msg,(error) => {
+        // enable button
+        $messageFormButton.removeAttribute('disabled');
+        $messageFormInput.value = '';
+        $messageFormInput.focus();
         if(error){
             return console.log(error);
         }
@@ -12,19 +29,18 @@ document.querySelector('#chat-form').addEventListener('submit',(e) => {
     });
 })
 
-socketio.on('message',(updatedMessage) => {
-    console.log(updatedMessage)
-})
 
-document.querySelector('#send-location').addEventListener('click',()=>{
+$sendLocationButton.addEventListener('click',()=>{
     if(!navigator.geolocation){
         return alert('Geolocation is not supported by your browser')
-    }
+    } $sendLocationButton.setAttribute('disabled','disabled')
     navigator.geolocation.getCurrentPosition((position) => {
+       
         socketio.emit('sendLocation',{
             latitude:position.coords.latitude,
             longitude:position.coords.longitude
         },()=>{
+            $sendLocationButton.removeAttribute('disabled')
             console.log('Location shared!')
         })
         console.log(position)
